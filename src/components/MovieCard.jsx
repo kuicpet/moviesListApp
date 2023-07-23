f6602af2d6ec4da8f4c807b6b602852ff2872e2e
kingsley umujeyan <kingsleyumujeyan@gmail.com>
2023-07-23 20:49:05 +0100

    ft--updated

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -27,15 +27,17 @@ const MovieCard = ({
         <h2 className='font-semibold capitalize'>{title}</h2>
         <p>{releaseDate ? `Release Year : ${releaseDate}` : ''}</p>
         {retable && (
-          <label className='flex justify-center items-center space-x-2 cursor-pointer px-2'>
-            <input
-              className='mx-1 form-checkbox h-3 w-4 text-blue-500 focus:ring-blue-400 focus:border-blue-400 border border-gray-300 rounded'
-              type='checkbox'
-              checked={title.watched}
-              onChange={() => handleWatchedChange()}
-            />
-            <span className='text-sm'> Watched</span>
-          </label>
+          <div className=''>
+            <label className='flex justify-center items-center space-x-2 cursor-pointer px-2'>
+              <input
+                className='mx-1 form-checkbox h-3 w-4 text-blue-500 focus:ring-blue-400 focus:border-blue-400 border border-gray-300 rounded'
+                type='checkbox'
+                checked={title.watched}
+                onChange={() => handleWatchedChange()}
+              />
+              <span className='text-sm'> Watched</span>
+            </label>
+          </div>
         )}
       </div>
       {clickable && (
